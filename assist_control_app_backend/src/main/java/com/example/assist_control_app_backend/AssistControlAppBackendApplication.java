ba3a8f9f91fd1a0d9cfd5140ed8e4a9419f25618
juan muñoz <masterjutox@gmail.com>
2023-08-04 02:24:54 -0400

    Modificacion

---
+++ assist_control_app_backend/src/main/java/com/example/assist_control_app_backend/AssistControlAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.assist_control_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistControlAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistControlAppBackendApplication.class, args);
+	}
+
+}
---
+++ assist_control_app_backend/src/test/java/com/example/assist_control_app_backend/AssistControlAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assist_control_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistControlAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
